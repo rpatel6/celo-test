2ac5a2b3bdb77ba99e9e2490a33d199f15d6ce26
Raj <raj1992_patel@windowslive.com>
2020-04-16 11:17:10 +1200

    Initial Commit

---
+++ Celo test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Celo test
+//
+//  Created by Raj Patel on 16/04/20.
+//  Copyright © 2020 Raj Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
